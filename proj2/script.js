a43e4761ab20ba3334c6b7427e40f735200684a7
Spencer Hirsch <spencer@yellowcrest.com>
2024-02-14 23:21:02 -0500

    working on setting up cards.

--- proj2/script.js
+++ proj2/script.js
@@ -4,9 +4,17 @@ fetch(filePath)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
-        console.log(data);
+        // console.log(data);
         const items = data.querySelectorAll("item");
+        let html = ``;
         items.forEach(item => {
-            console.log(item)
-        })
+            // console.log(item)
+            html += `
+                <article>
+                    <img src="${item.querySelector("link").innerHTML}">
+                </article>
+            `;
+        });
+        let targetElement = document.querySelector('.projects-body');
+        targetElement.insertAdjacentHTML('beforeend', html);
     });
