5d1523e6a93fee3d49f810586c57f578dbf19e3c
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2024-02-19 18:40:43 -0500

    Filter by date

--- proj2/js/script.js
+++ proj2/js/script.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
     const filePath = "data/events.rss";
-    let eventList = [];
+    const eventList = [];
 
     fetch(filePath)
         .then(response => response.text())
@@ -14,26 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 const img = enclosureElement ? enclosureElement.getAttribute("url") : "N/A";
 
                 const title = el.querySelector("title").textContent;
-                const tempDate = el.querySelector("start").textContent;
-                
-                /* Debugging to make sure I grabbed it all correctly
-                console.log("Image src:", img);
-                console.log("Title:", title);
-                console.log("Start Date: ", formattedStartDate);
-                console.log("Location:", location);
-                console.log("Description: ", description);
-                */
-
-                // Convert date to MMMM, dd, YYYY, TZD
-                const startDate = new Date(tempDate);
-                const options = {
-                    month: 'long',
-                    day: 'numeric',
-                    year: 'numeric',
-                    timeZoneName: 'short'
-                };
-
-                const formattedStartDate = startDate.toLocaleDateString('en-US', options);
+                const startDate = formatDate(el.querySelector("start").textContent);
 
                 const location = el.querySelector("location").textContent;
                 const description = el.querySelector("description").textContent;
@@ -41,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 const event = {
                     img: img,
                     title: title,
-                    date: formattedStartDate,
+                    date: startDate,
                     location: location,
                     desc: description
                 };
@@ -51,16 +34,30 @@ document.addEventListener('DOMContentLoaded', function() {
             generateArticles(eventList);
         });
 
+
+    // Convert date to MMMM, dd, YYYY, TZD
+    function formatDate(text) {
+        const startDate = new Date(text);
+        const options = {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+            timeZoneName: "short"
+        };
+
+        return startDate.toLocaleDateString("en-US", options);
+    }
+
     function toggleDescription(event) {
         event.preventDefault();
         const description = event.target.nextElementSibling;
-        description.classList.toggle('visible');
+        description.classList.toggle("visible");
     }
 
     function generateArticles(eventList) {
         console.log(eventList);
         console.log(eventList[0]);
-        let html = '';
+        let html = "";
         eventList.forEach(event => {
             html += `
                 <article class="card">
@@ -73,39 +70,60 @@ document.addEventListener('DOMContentLoaded', function() {
                 </article>
             `;
         });
-        const ProjectsBody = document.getElementById('cardId');
+
+        const ProjectsBody = document.getElementById("cardId");
         ProjectsBody.innerHTML = html;
 
-        ProjectsBody.addEventListener('click', function (event) {
-            if (event.target.classList.contains('learn-more-link')) {
+        ProjectsBody.addEventListener("click", function (event) {
+            if (event.target.classList.contains("learn-more-link")) {
                 toggleDescription(event);
             }
         });
-
-        // submit button listener
-        const submitButton = document.getElementById('submit');
-        submitButton.addEventListener('click', function() {
-            // Get filter values from input fields
-            const descriptionFilterValue = document.getElementById('description-id').value.trim();
-
-            console.log(descriptionFilterValue);
-            
-            // Filter events based on input values
-            let filteredEvents = eventList;
-            filteredEvents = eventFilter(filteredEvents, descriptionFilterValue, filterByDesc);
-
-            generateArticles(filteredEvents);
-        });
     }
+
+    // submit button listener
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", function () {
+        // Filter parameters
+        const filterObj = {
+            "title": document.getElementById("title-id").value.trim(),
+            "startDate": document.getElementById("date-id").value.trim(),
+            "description": document.getElementById("description-id").value.trim()
+        };
+
+        // Filter events based on input values
+        let resultObjs = eventList;
+        resultObjs = eventFilter(resultObjs, filterObj.title, titleFilter);
+        resultObjs = eventFilter(resultObjs, filterObj.startDate, dateFilter);
+        resultObjs = eventFilter(resultObjs, filterObj.description, descriptionFilter);
+        generateArticles(resultObjs);
+    });
 
     // eventFilter function
     function eventFilter(events, filterValue, filterFunction) {
-        if (!filterValue) return events; 
+        if (!filterValue) return events;
         return events.filter(event => filterFunction(event, filterValue));
     }
 
+    // Filtering events based on title
+    function titleFilter(event, filterValue) {
+        // TODO
+        return true;
+    }
+
+    // Filtering events based on date
+    function dateFilter(event, filterValue) {
+        const filterDate = new Date(filterValue);
+        const eventDate = new Date(event.date.replace("at EST", ""));
+
+        // Day/month/year must match
+        return filterDate.getDate() == eventDate.getDate()
+            && filterDate.getMonth() == eventDate.getMonth()
+            && filterDate.getFullYear() == eventDate.getFullYear();
+    }
+
     // filtering events by description
-    function filterByDesc(event, filterValue) {
+    function descriptionFilter(event, filterValue) {
         return event.desc.toLowerCase().includes(filterValue.toLowerCase());
     }
 });
