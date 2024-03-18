87a74202542993dcb216768a89d2ab33033e6ff9
Thomas <thomasjohnso2020@my.fit.edu>
2024-03-18 13:34:44 -0400

    Display customers, need to style it now

--- proj3/src/App.js
+++ proj3/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react'
 import Header from "./Header";
 import ProductGrid from './components/ProductGrid';
+import CustomerTable from './components/CustomerTable';
 
 function App() {
   return (
       <div className='App'>
         <Header />
-        <ProductGrid />
+        <CustomerTable />
       </div>
   );
 }
---
+++ proj3/src/components/CustomerTable.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './CustomerTableStyle.css';
+
+const CustomerTable = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('customers.json') // Placed data in /public/ directory because you cannot import from outside src, use fetch to get it from public
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setItems(data);
+      })
+      .catch(error => console.error("Failed to load products.json:", error));
+  }, []);
+
+  return (
+    <table className="customer-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Email</th>
+          <th>Revenue</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index) => (
+          <tr key={item.login.uuid}>
+            <td>{`${item.name.title} ${item.name.first} ${item.name.last}`}</td>
+            <td>{`${item.location.street.number} ${item.location.street.name}, ${item.location.city}, ${item.location.state}, ${item.location.country}, ${item.location.postcode}`}</td>
+            <td>{item.email}</td>
+            <td>TBA</td>
+            <td><img src={item.picture.thumbnail} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} /></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CustomerTable;
